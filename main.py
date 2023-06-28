2005090ce484185289abdee6288c0ae6a27672b7
George Astakhov <goha1115@mail.ru>
2023-06-28 18:01:19 +0300

    added comments to main.py

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 from utils.fixtures.settings import load_json, OPERATIONS_JSON, delete_blank_data, get_first_five_sorted_operations, \
     get_five_sorted_classes
 
-
+# Основная функция
 def main():
+    # получаем список со словарями
     data = load_json(OPERATIONS_JSON)
-
+    # получаем обработанный список
     new_data = delete_blank_data(data)
-
+    # выбераем 5 последних операций
     five_operations = get_first_five_sorted_operations(new_data)
-
+    # получаем 5 обработанных эземпляров классов последних пяти операций
     new_five_operations = get_five_sorted_classes(five_operations)
-
+    # выводим статистику
     for operation in new_five_operations:
         print(operation)
 
